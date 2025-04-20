3772a08a219476fa4b2d1d73fb91b18962e06c68
Jeronimo9293 <jerogarciadelrio@gmail.com>
2025-04-20 20:24:02 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './firebase.js';
+import './login.js';
+import './signup.js';
+import './export.js';
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("Human OS loaded");
+});
