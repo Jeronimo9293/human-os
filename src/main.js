3304be81cf3972f557a15614caeb83827ee5b347
Jeronimo9293 <jerogarciadelrio@gmail.com>
2025-04-20 19:58:42 +0100

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './firebase.js';
-import './login.js';
-import './signup.js';
-import './export.js';
-
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("Human OS loaded");
-});
